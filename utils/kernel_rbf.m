78d57ba6d4729c3b92bb232728c8a350762e2bab
pejotr <piotr.doniec.pl@gmail.com>
2012-02-08 10:57:11 +0100

    Update utils/kernel_rbf.m

--- utils/kernel_rbf.m
+++ utils/kernel_rbf.m
@@ -1,14 +1,14 @@
-function k = kernel_rbf(u, v, sigma)
-    [r1, c1] = size(u);
-    [r2, c2] = size(v);
+#function k = kernel_rbf(u, v, sigma)
+#    [r1, c1] = size(u);
+#    [r2, c2] = size(v);
    
-    vr = v;
+#    vr = v;
     
-    if r1 ~= r2
-        vr = repmat(v, r1, 1);
-    end
+#    if r1 ~= r2
+#        vr = repmat(v, r1, 1);
+#    end
     
-    u
-    vr
+#    u
+#    vr
     
-    k = exp(-sigma*norm(vr-u)^2);
+#    k = exp(-sigma*norm(vr-u)^2);
